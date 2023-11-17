71bf0043830941fd1ff41f6525237a0a840fe91a
bmilde <bmilde@M>
2023-11-17 18:54:17 +0200

    added path

--- fix_import.sh
+++ fix_import.sh
@@ -1,5 +1,5 @@
 # These two find+sed commands prepend "espnet_streaming_decoder" to imports of modules in either espnet or espnet2.
 # While all files in this repository are already patched this way, this could be very useful if you are updating files from espnet.
 
-find /path/to/project -name "*.py" -exec sed -i 's/from \(espnet\.\)/from espnet_streaming_decoder.\1/gI; s/import \(espnet\.\)/import espnet_streaming_decoder.\1/gI' {} +
-find /path/to/project -name "*.py" -exec sed -i 's/from \(espnet2\.\)/from espnet_streaming_decoder.\1/gI; s/import \(espnet2\.\)/import espnet_streaming_decoder.\1/gI' {} +
+find espnet_streaming_decoder/ -name "*.py" -exec sed -i 's/from \(espnet\.\)/from espnet_streaming_decoder.\1/gI; s/import \(espnet\.\)/import espnet_streaming_decoder.\1/gI' {} +
+find espnet_streaming_decoder/ -name "*.py" -exec sed -i 's/from \(espnet2\.\)/from espnet_streaming_decoder.\1/gI; s/import \(espnet2\.\)/import espnet_streaming_decoder.\1/gI' {} +
